0b77194e4ca1599b475813a5afa94205276ee5e6
Vinayak <141934650+gargvinayak2005@users.noreply.github.com>
2025-09-04 19:20:12 +0530

    fix: set Vite base for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Agriculture-crop-doctor-2/',
+  base: '/Agriculture-crop-doctor/',
   server: {
     port: 4000,
     host: true
